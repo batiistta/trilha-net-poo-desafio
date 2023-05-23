08f6a5a65a2d1a19f72b4de480e47e1a21063e0f
gabriel <gbbatistasouza@gmail.com>
2023-05-22 21:59:29 -0300

    Commit Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia1 = new Nokia(numero: "4877", modelo:"15s", imei: "4587441", memoria: 256);
+
+nokia1.InstalarAplicativo("Facebook");
+nokia1.Ligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Iphone iphone1 = new Iphone(numero: "1542", modelo:"X", imei: "15154151", memoria: 128);
+
+
+iphone1.InstalarAplicativo("Instagram");
+iphone1.ReceberLigacao();
+
